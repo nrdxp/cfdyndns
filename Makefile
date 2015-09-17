a137f510888fb9a71a78f0f28a4ab17dcb273dec
Cole Mickens <cole.mickens@gmail.com>
2015-09-17 00:36:21 -0700

    fix build,

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
+APIKEY:=`cat apikey.md`
+
 run:
-	CLOUDFLARE_APIKEY=$(cat apikey.md) CLOUDFLARE_EMAIL=cole.mickens@gmail.com cargo run
+	CLOUDFLARE_APIKEY=$(APIKEY) CLOUDFLARE_EMAIL=cole.mickens@gmail.com cargo run --verbose
--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,25 @@ extern crate serde;
 extern crate serde_json;
 
 use std::env;
-use std::io;
 use std::io::Read;
 
 use hyper::Client;
 use hyper::header::Connection;
-use hyper::header::Headers;
-use hyper::client::RequestBuilder;
 
 use serde_json::value::*;
 
 header! { (XAuthKey, "X-Auth-Key") => [String] }
 header! { (XAuthEmail, "X-Auth-Email") => [String] }
 
+// TODO(colemickens): none of the implementations handle paging properly
+
 fn main() {
     env_logger::init().unwrap();
 
-    let cloudflare_apikey = env::var("cloudflare_apikey").unwrap();
-    let cloudflare_email = env::var("cloudflare_email").unwrap();
+    let cloudflare_apikey = env::var("CLOUDFLARE_APIKEY").expect("missing apikey");
+    let cloudflare_email = env::var("CLOUDFLARE_EMAIL").expect("missing email");
 
-    let zonesUrl = "https://api.cloudflare.com/client/v4/zones";
+    let zones_url = "https://api.cloudflare.com/client/v4/zones";
 
     let client = Client::new();
 
@@ -33,24 +32,24 @@ fn main() {
     auth_headers.set(XAuthKey(cloudflare_apikey.to_owned()));
     auth_headers.set(XAuthEmail(cloudflare_email.to_owned()));
 
-    let mut res = client
-        .get(zonesUrl)
+    let res = client
+        .get(zones_url)
         .header(Connection::close())
         .headers(auth_headers)
         .send()
         .unwrap();
 
-    // Read the Response.
-    let mut body = String::new();
-    //res.read_to_string(&mut body).unwrap();
-
-    // serde
-    // let json = json::from_str(body).unwrap();
-    // let json: serde_json::value::Value = serde_json::from_str(&body).unwrap();
-    
     let json: serde_json::value::Value = serde_json::from_iter(res.bytes()).unwrap();
 
-    let zoneIds = json
+    let status = json
+        .as_object().unwrap()
+        .get("success").unwrap()
+        .as_boolean().unwrap();
+    if !status {
+        println!("failed request!");
+    }
+
+    let zone_ids = json
         .as_object().unwrap()
         .get("result").unwrap()
         .as_array().unwrap()
@@ -58,11 +57,10 @@ fn main() {
         .map(|ref id| id.find("id").unwrap()
             .as_string().unwrap());
 
-    for id in zoneIds {
+    for id in zone_ids {
         println!("{}", id);
     }
 
-    println!("Response: {}", res.status);
-    println!("Headers:\n{}", res.headers);
-    io::copy(&mut res, &mut io::stdout()).unwrap();
+    // println!("Response: {}", res.status);
+    // println!("Headers:\n{}", res.headers);
 }
