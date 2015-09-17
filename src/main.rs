3a8785874af13801878f685047b07cdbc4cc0ac9
Cole Mickens <cole.mickens@gmail.com>
2015-09-17 00:49:32 -0700

    reorganize for great success

--- src/main.rs
+++ src/main.rs
@@ -23,44 +23,44 @@ fn main() {
     let cloudflare_apikey = env::var("CLOUDFLARE_APIKEY").expect("missing apikey");
     let cloudflare_email = env::var("CLOUDFLARE_EMAIL").expect("missing email");
 
-    let zones_url = "https://api.cloudflare.com/client/v4/zones";
-
     let client = Client::new();
 
     let mut auth_headers = hyper::header::Headers::new();
-
     auth_headers.set(XAuthKey(cloudflare_apikey.to_owned()));
     auth_headers.set(XAuthEmail(cloudflare_email.to_owned()));
 
-    let res = client
+    let zones_url = "https://api.cloudflare.com/client/v4/zones";
+    let mut zones_response = client
         .get(zones_url)
         .header(Connection::close())
         .headers(auth_headers)
-        .send()
-        .unwrap();
+        .send().unwrap();
 
-    let json: serde_json::value::Value = serde_json::from_iter(res.bytes()).unwrap();
+    if zones_response.status != hyper::status::StatusCode::Ok {
+         println!("she's gonna blow!");
+         println!("exit early"); // TODO(colemickens): fix
+    }
 
-    let status = json
+    let zones_json: serde_json::value::Value = serde_json::from_iter(zones_response.by_ref().bytes()).unwrap();
+
+    let status = zones_json
         .as_object().unwrap()
         .get("success").unwrap()
         .as_boolean().unwrap();
     if !status {
-        println!("failed request!");
+        println!("request sent 200, but cloudflare reported !success"); // TODO(colemickens): this is inaccurate due to above "fix" todo
     }
 
-    let zone_ids = json
+    let zone_ids = zones_json
         .as_object().unwrap()
         .get("result").unwrap()
         .as_array().unwrap()
         .iter()
-        .map(|ref id| id.find("id").unwrap()
+        .map(|ref id|
+            id.find("id").unwrap()
             .as_string().unwrap());
 
     for id in zone_ids {
         println!("{}", id);
     }
-
-    // println!("Response: {}", res.status);
-    // println!("Headers:\n{}", res.headers);
 }
