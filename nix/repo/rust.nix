9cc9c6f7432f5e287d57d3bbe3aab568e23107f4
Timothy DeHerrera <tim@nrdxp.dev>
2023-08-30 17:52:22 -0600

    feat: add Standard Nix framework integration
    Provides a working devshell and package binary.

---
+++ nix/repo/rust.nix
@@ -0,0 +1,23 @@
+# export fenix toolchain as its own package set
+{
+  inputs,
+  cell,
+}: let
+  inherit (inputs) fenix;
+
+  # you may change "default" to any of "[minimal|default|complete|latest]" for variants
+  # see upstream fenix documentation for details
+  rustPkgs = builtins.removeAttrs fenix.packages.complete ["withComponents" "name" "type"];
+in
+  # add rust-analyzer from nightly, if not present
+  if rustPkgs ? rust-analyzer
+  then rustPkgs
+  else
+    rustPkgs
+    // {
+      inherit (fenix.packages) rust-analyzer;
+      toolchain = fenix.packages.combine [
+        (builtins.attrValues rustPkgs)
+        fenix.packages.rust-analyzer
+      ];
+    }
